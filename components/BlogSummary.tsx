31b7621aa9ab6ccbe854922b7d428c59c029bd37
lonelyLob0 <abab1000@gmail.com>
2024-04-10 20:00:14 -0600

    Icon to Navbar has been added and Tags layout improved

--- components/BlogSummary.tsx
+++ components/BlogSummary.tsx
@@ -6,11 +6,17 @@ import { getPostTags } from "@/actions";
 export default async function BlogSummary({ blog }: { blog: Post }) {
   const tags = await getPostTags(blog.id);
 
+  const blogDate = blog.updatedAt.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
+
   return (
     <article className={classes["blog-summary"]}>
       <section className={classes.main}>
         <p className={classes.description}>{blog.title}</p>
-        <p className={classes["creation-date"]}>Created on 14/05/2024</p>
+        <p className={classes["creation-date"]}>{blogDate}</p>
       </section>
       <section className={classes.footer}>
         {tags && tags.map((tag) => <BlogTag key={tag.id} name={tag.name} />)}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,13 @@
 import classes from "@/components/Navbar.module.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWolfPackBattalion } from "@fortawesome/free-brands-svg-icons";
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <header className={classes.header}>
+      <FontAwesomeIcon icon={faWolfPackBattalion} className={classes.icon} />
       <ul className={classes.navigation}>
         <li>Home</li>
         <li>Login</li>
